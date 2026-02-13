4f2bb450b4a7e9099360f68bf498bd878bf6e167
colin <163393924+officialcc@users.noreply.github.com>
2026-02-13 16:25:44 +0800

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some Address, Some City',
+        description: 'This is a first meetup!"'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Another Address, Another City',
+        description: 'This is a second meetup!"'
+    },
+]
+
+function HomePage() {
+    return (
+        <MeetupList meetups={DUMMY_MEETUPS} />
+    );
+}
+
+export default HomePage;
